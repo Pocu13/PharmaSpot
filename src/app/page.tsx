6764f819676f3c6f17801206ddc491730f1fd296
Pocu13 <pocu18@gmail.com>
2025-06-03 18:23:32 +0200

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import dynamic from 'next/dynamic';
-
-const FarmaciInterface = dynamic(() => import('../components/pharmspot-interface'), { ssr: false });
-
-export default function Home() {
-  return <FarmaciInterface />;
-}
